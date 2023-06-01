9e7bf31ce8bc5d86ee309a556fcce331897236b0
KurisuSF <cris.fut7@hotmail.com>
2023-06-01 00:26:23 -0700

    kk

--- proyectos_finales/traffic_sign_recognition/main.py
+++ proyectos_finales/traffic_sign_recognition/main.py
@@ -30,6 +30,28 @@ for i in range(classes):
         except:
            print("Error loading image")
            
+# folders = os.listdir(path)
+
+# train_number = []
+# class_num = []
+
+# for folder in folders:
+#     train_files = os.listdir(path + '/' + folder)
+#     train_number.append(len(train_files))
+#     class_num.append(classes[int(folder)])
+    
+# # Sorting the dataset on the basis of number of images in each class
+# zipped_lists = zip(train_number, class_num)
+# sorted_pairs = sorted(zipped_lists)
+# tuples = zip(*sorted_pairs)
+# train_number, class_num = [ list(tuple) for tuple in  tuples]
+
+# # Plotting the number of images in each class
+# plt.figure(figsize=(21,10))  
+# plt.bar(class_num, train_number)
+# plt.xticks(class_num, rotation='vertical')
+# plt.show() 
+
 #Converting lists into numpy arrays
 data = np.array(data)
 labels = np.array(labels)
